8ca58c0e4b42485e20f78b6f88207e4234f76f88
Prasanth Ullattil <prasanth@telenordigital.com>
2019-12-04 10:48:47 +0100

    Some IO tests

---
+++ src/test/kotlin/org/study/arrow/MiscTests.kt
@@ -0,0 +1,48 @@
+package org.study.arrow
+
+import arrow.core.Either
+import arrow.core.extensions.fx
+import arrow.core.getOrElse
+import arrow.core.right
+import arrow.fx.IO
+import arrow.fx.extensions.fx
+import arrow.fx.fix
+import org.junit.Test
+
+class MiscTests {
+    object KnownError
+
+    private fun getEitherValue(): Either<KnownError, Int> {
+        return 1.right()
+    }
+    private fun getEitherValueWithException(): Either<KnownError, Int> {
+        throw RuntimeException("Catch me")
+    }
+
+    @Test
+    fun testEitherFx() {
+        val result: Either<KnownError, Int> = Either.fx {
+            getEitherValue().getOrElse { -1 }
+        }
+        assert(result is Either.Right)
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun testEitherFxException() {
+        val result: Either<KnownError, Int> = Either.fx {
+            getEitherValueWithException().getOrElse { -1 }
+        }
+        assert(result is Either.Right)
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun testIO() {
+        val r = IO.fx {
+            val result: Either<KnownError, Int> = Either.fx {
+                getEitherValueWithException().getOrElse { -1 }
+            }
+            result
+        }.fix().unsafeRunSync()
+        assert(r is Either.Right)
+    }
+}
